3a8a4c9f022cc4b10abf3cb38d4d96ba21322bd7
Bruce P <bruce@binarywebpark.com>
2013-05-15 22:49:18 -0700

    added larubysg branch

--- mad_libs.rb
+++ mad_libs.rb
@@ -1,19 +1,15 @@
-def get_token_array(sentence)
-  # ask for user input and subsitute
-  #Example:I had a ((an adjective)) sandwich for lunch today. It dripped all over my ((a body part)) and ((a noun)).
-  token_arr = sentence.scan(/\(\(.*\)\)/)
+sentence_library=["I had a ((an adjective)) sandwich for lunch today. It dripped all over my ((a body part)) and ((a noun)).",
+  "Our favorite language is ((gem:a gemstone)). We think ((gem)) is better than ((a gemstone))."]
+#token_arr = sentence.scan(/\(\(([^)]+)\)\)/)
+sentence = sentence_library[rand(sentence_library.size)]
+token_arr = sentence.scan(/\(\(.*?\)\)/)
+
+hsh = {}
+token_arr.each do |t|
+  puts "Enter a word for #{t.delete('()')}?"
+  hsh[t] = gets.chomp
 end
-def get_user_input(token)
-  puts "Enter a word or phrase to substitute for #{token}."
-  sub_str = gets
-  sub_str
+hsh.each do |key,val|
+  sentence.gsub!(key,val)
 end
-def hash_substitutes(token_arr)
-  hash_sub = {}
-  token_arr.each do |t|
-    hash_sub[t] = get_user_input(t)
-  end
-  hash_sub
-end
-p get_token_array("I had a ((an adjective)) sandwich for breakfast today. It oozed all
-over my ((a body part)) and ((a noun)).")
+p sentence
